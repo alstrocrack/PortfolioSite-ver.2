1b96a3e87affa58519c6c441a864f1163e6b8fc4
yuto urushima <yuto259233@gmail.com>
2021-02-13 22:45:05 +0900

    sass コンパイル

--- gulpfile.js
+++ gulpfile.js
@@ -1,1 +1,26 @@
+// 定数の設定
+const {src, dest, watch, series, parallel } = require('gulp');
 const sass = require('gulp-sass');
+const plumber = require('gulp-plumber');
+const notify = require('gulp-notify');
+
+// パスの設定
+const paths = {
+    root: 'dist/',
+    sass: 'src/assets/styles/**/*.scss',
+    css: 'dist/assets/styles/',
+}
+
+// sass
+const css = () => {
+    return src(paths.sass)
+      .pipe(
+        plumber({ errorHandler: notify.onError('Error: <%= error.message %>') })
+      )
+      .pipe(sass())
+      .pipe(dest(paths.css))
+}
+
+exports.default = () => {
+    watch(paths.sass, css)
+}
